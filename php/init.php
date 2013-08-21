132792b89c49de1beea0d3fd8bbb8929146a89e6
Wang Jing <wangjild@baidu.com>
2013-08-21 15:10:19 +0800

    add basic search

---
+++ php/init.php
@@ -0,0 +1,26 @@
+<?php
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file init.php
+ * @author wangjing14(com@baidu.com)
+ * @date 2013/08/21 13:50:31
+ * @brief 
+ *  
+ **/
+
+if (!extension_loaded('curl.so')) {
+    trigger_error('未加载curl扩展，请先安装curl扩展', USER_ERROR);
+}
+
+define ('ROOT_PATH', dirname(__FILE__));
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+?>
